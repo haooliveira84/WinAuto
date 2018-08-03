c4477f5857ade103ddc0db4b43dcb30f30e7d450
Rohan Scanavez <rohangustavo@gmail.com>
2018-08-03 14:48:41 -0300

    Incluido script para alteração de senha de todos os usuarios de uma OU

---
+++ Diarios/AlteraSenhaOU.ps1
@@ -0,0 +1,20 @@
+# Converter para string
+$pwd = "senha_desejada"
+$password = "senha_desejada"
+$pwd = convertto-securestring $password -asplaintext -force
+
+
+
+# Mostrar senha gerada na console
+$password
+
+$users = Get-ADUser -filter * -searchbase "D. NAME DA OU QUE CONTEM OS USUARIOS"
+echo "COUNT:"
+($users | measure).Count
+ 
+foreach ($user in $users)
+    {
+     Set-ADAccountPassword -Identity $user.SAMAccountName -NewPassword $pwd -Reset
+     $usuario_alterado = $user.UserPrincipalName
+     "Senha de $usuario_alterado alterada para $password"
+    }
